b0f74b9479e7dd756fa0fb28f4f418d032863b73
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-12-29 08:49:23 +0530

    feat: Display message for logged-in users accessing login/signup routes
    Show a message on the login and signup pages to inform users that they are already authenticated when attempting to access these routes.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,12 +7,12 @@ import Card from "@mui/material/Card";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { Typography } from "@mui/material";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const setUser = useSetRecoilState(userState);
+    const [user, setUser] = useRecoilState(userState);
     const navigate = useNavigate();
 
     const newLogin = () => {
@@ -36,50 +36,62 @@ function Login() {
                 navigate("/courses");
             });
     };
-
-    return (
-        <div
-            style={{
-                width: "100%",
-                height: "100vh",
-                backgroundColor: "#eeeeee",
-                paddingTop: "20vh",
-            }}
-        >
-            <center>
-                <Typography variant="h5">Welcome back! Login here.</Typography>
-                <br />
-                <Card variant="outlined" style={{ width: 400, padding: "2%" }}>
-                    <TextField
-                        fullWidth
-                        label="Email"
-                        variant="outlined"
-                        type={"text"}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <br />
-                    <br />
-                    <TextField
-                        fullWidth
-                        label="Password"
-                        variant="outlined"
-                        type={"text"}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <br />
-                    <br />
-                    <Button variant="contained" onClick={newLogin}>
-                        Login
-                    </Button>
-                    <br />
-                    <br />
-                    <Typography fontSize={"caption"} variant="h6">
-                        New user? <a href="/register">Register</a>
+    if (user.userEmail) {
+        return (
+            <Typography variant="h4" align="center" style={{ marginTop: 160 }}>
+                You're already logged in.
+            </Typography>
+        );
+    } else {
+        return (
+            <div
+                style={{
+                    width: "100%",
+                    height: "100vh",
+                    backgroundColor: "#eeeeee",
+                    paddingTop: "20vh",
+                }}
+            >
+                <center>
+                    <Typography variant="h5">
+                        Welcome back! Login here.
                     </Typography>
-                </Card>
-            </center>
-        </div>
-    );
+                    <br />
+                    <Card
+                        variant="outlined"
+                        style={{ width: 400, padding: "2%" }}
+                    >
+                        <TextField
+                            fullWidth
+                            label="Email"
+                            variant="outlined"
+                            type={"text"}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <br />
+                        <br />
+                        <TextField
+                            fullWidth
+                            label="Password"
+                            variant="outlined"
+                            type={"text"}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <br />
+                        <br />
+                        <Button variant="contained" onClick={newLogin}>
+                            Login
+                        </Button>
+                        <br />
+                        <br />
+                        <Typography fontSize={"caption"} variant="h6">
+                            New user? <a href="/register">Register</a>
+                        </Typography>
+                    </Card>
+                </center>
+            </div>
+        );
+    }
 }
 
 export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -7,13 +7,13 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 
 function Register() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
-    const setUser = useSetRecoilState(userState);
+    const [user, setUser] = useRecoilState(userState);
 
     const newRegister = () => {
         axios
@@ -31,51 +31,62 @@ function Register() {
             });
     };
 
-    return (
-        <div
-            style={{
-                width: "100%",
-                height: "100vh",
-                backgroundColor: "#eeeeee",
-                paddingTop: "20vh",
-            }}
-        >
-            <center>
-                <Typography variant="h5">
-                    Welcome to COURZERO! Sign up here.
-                </Typography>
-                <br />
-                <Card variant="outlined" style={{ width: 400, padding: "2%" }}>
-                    <TextField
-                        fullWidth
-                        label="Email"
-                        variant="outlined"
-                        type={"text"}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <br />
-                    <br />
-                    <TextField
-                        fullWidth
-                        label="Password"
-                        variant="outlined"
-                        type={"text"}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <br />
-                    <br />
-                    <Button variant="contained" onClick={newRegister}>
-                        Register
-                    </Button>
-                    <br />
-                    <br />
-                    <Typography fontSize={"caption"} variant="h6">
-                        Already a user? <a href="/login">Login</a>
+    if (user.userEmail) {
+        return (
+            <Typography variant="h4" align="center" style={{ marginTop: 160 }}>
+                You've already registered.
+            </Typography>
+        );
+    } else {
+        return (
+            <div
+                style={{
+                    width: "100%",
+                    height: "100vh",
+                    backgroundColor: "#eeeeee",
+                    paddingTop: "20vh",
+                }}
+            >
+                <center>
+                    <Typography variant="h5">
+                        Welcome to COURZERO! Sign up here.
                     </Typography>
-                </Card>
-            </center>
-        </div>
-    );
+                    <br />
+                    <Card
+                        variant="outlined"
+                        style={{ width: 400, padding: "2%" }}
+                    >
+                        <TextField
+                            fullWidth
+                            label="Email"
+                            variant="outlined"
+                            type={"text"}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <br />
+                        <br />
+                        <TextField
+                            fullWidth
+                            label="Password"
+                            variant="outlined"
+                            type={"text"}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <br />
+                        <br />
+                        <Button variant="contained" onClick={newRegister}>
+                            Register
+                        </Button>
+                        <br />
+                        <br />
+                        <Typography fontSize={"caption"} variant="h6">
+                            Already a user? <a href="/login">Login</a>
+                        </Typography>
+                    </Card>
+                </center>
+            </div>
+        );
+    }
 }
 
 export default Register;
