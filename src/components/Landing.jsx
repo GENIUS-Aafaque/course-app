a317691d1977ff07c40c91a98f6bfbf4c5337cef
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2023-12-29 08:04:17 +0530

    Add conditional rendering for signup and signin buttons
    Added conditions to render signup and signin buttons only when user loading is complete and no user email is available.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,15 @@
 import { Grid, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
+import { userEmailState } from "../store/selectors/userEmail";
+import { useRecoilValue } from "recoil";
+import { isUserLoading } from "../store/selectors/isUserLoading";
 
 export const Landing = () => {
     const navigate = useNavigate();
+    const userEmail = useRecoilValue(userEmailState);
+    const userLoading = useRecoilValue(isUserLoading);
+
     return (
         <div>
             <Grid container style={{ padding: "5vw" }}>
@@ -13,30 +19,32 @@ export const Landing = () => {
                         <Typography variant={"h5"}>
                             A place to learn from Zero and become a Hero
                         </Typography>
-                        <div style={{ display: "flex", marginTop: 20 }}>
-                            <div style={{ marginRight: 10 }}>
-                                <Button
-                                    size={"large"}
-                                    variant={"contained"}
-                                    onClick={() => {
-                                        navigate("/register");
-                                    }}
-                                >
-                                    Signup
-                                </Button>
+                        {!userLoading && !userEmail && (
+                            <div style={{ display: "flex", marginTop: 20 }}>
+                                <div style={{ marginRight: 10 }}>
+                                    <Button
+                                        size={"large"}
+                                        variant={"contained"}
+                                        onClick={() => {
+                                            navigate("/register");
+                                        }}
+                                    >
+                                        Signup
+                                    </Button>
+                                </div>
+                                <div>
+                                    <Button
+                                        size={"large"}
+                                        variant={"contained"}
+                                        onClick={() => {
+                                            navigate("/login");
+                                        }}
+                                    >
+                                        Signin
+                                    </Button>
+                                </div>
                             </div>
-                            <div>
-                                <Button
-                                    size={"large"}
-                                    variant={"contained"}
-                                    onClick={() => {
-                                        navigate("/login");
-                                    }}
-                                >
-                                    Signin
-                                </Button>
-                            </div>
-                        </div>
+                        )}
                     </div>
                     <div></div>
                 </Grid>
